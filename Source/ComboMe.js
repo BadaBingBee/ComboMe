f7ad49752b4c2a9f2219b4a9ce4ed3883a9d9540
BadaBingBee <bocomhrai@gmail.com>
2013-12-09 22:09:18 +0100

    Update ComboMe.js

--- Source/ComboMe.js
+++ Source/ComboMe.js
@@ -1,42 +1,44 @@
 (function ( $ ) {
-    $.fn.ComboMe = function() {
-        
-        this.button({    // give the button the down arrow.
-            text: true,
-            icons: {secondary: "ui-icon-triangle-1-s"}
-        })
-        .click(function() {   
-            var bt = $(this);                        // get the button
-            var menu = $( this ).parent().next();    // get the menu (the UL)
-            menu.width( bt.width()-3 );              // set the menu width to match the button width
-            
-            // styling        
-            menu.css("position", "absolute");
+$.fn.ComboMe = function() {
     
-            // for every anchor in the menu bind click to update the button text with the anchor text.
-            menu.find("li").each(function( index ) {
-                var link = $( this ).find("a");
-                link.bind( "click", function() {
-                    bt.find("span").text( $(this).text() );
-                });
-            });        
-            
-            // position the menu
-            menu.show().position({
-                my: "left top",
-                at: "left bottom",
-                of: this
+    this.button({    // give the button the down arrow.
+        text: true,
+        icons: {secondary: "ui-icon-triangle-1-s"}
+    })
+    .click(function() {   
+        var bt = $(this);                        // get the button
+        var menu = $( this ).parent().next();    // get the menu (the UL)
+        menu.width( bt.width()-3 );              // set the menu width to match the button width
+        
+        // styling        
+        menu.css("position", "absolute");
+
+        // for every anchor in the menu bind click to update the button text with the anchor text.
+        menu.find("li").each(function( index ) {
+            var link = $( this ).find("a");
+            link.bind( "click", function() {
+                var val = $(this).text();
+                bt.find("span").text( val );
+                bt.trigger("changed", val);
             });
-            // handle hiding the menu
-            $( document ).one( "click", function() {
-               menu.hide();
-            });
-            return false;
-        })
-        .parent()
-        .buttonset()
-        .next()
-        .hide()
-        .menu();
-    };
+        });        
+        
+        // position the menu
+        menu.show().position({
+            my: "left top",
+            at: "left bottom",
+            of: this
+        });
+        // handle hiding the menu
+        $( document ).one( "click", function() {
+           menu.hide();
+        });
+        return false;
+    })
+    .parent()
+    .buttonset()
+    .next()
+    .hide()
+    .menu();      
+};
 }( jQuery ));
