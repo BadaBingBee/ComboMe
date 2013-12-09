baffea1b599f02a817065e55913f59a799fbb2c1
BadaBingBee <bocomhrai@gmail.com>
2013-12-09 17:21:59 +0100

    Create ComboMe.js

---
+++ Source/ComboMe.js
@@ -0,0 +1,37 @@
+$.fn.ComboMe = function() {
+    
+    this.button({    // give the button the down arrow.
+        text: true,
+        icons: {secondary: "ui-icon-triangle-1-s"}
+    })
+    .click(function() {   
+        var bt = $(this);                        // get the button
+        var menu = $( this ).parent().next();    // get the menu
+        menu.width( bt.width()-3 );              // set the menu width to match the button width
+
+        // for every anchor in the menu bind click to update the button text with the anchor text.
+        menu.find("li").each(function( index ) {
+            var link = $( this ).find("a");
+            link.bind( "click", function() {
+                bt.find("span").text( $(this).text() );
+            });
+        });        
+        
+        // position the menu
+        menu.show().position({
+            my: "left top",
+            at: "left bottom",
+            of: this
+        });
+        // handle hiding the menu
+        $( document ).one( "click", function() {
+           menu.hide();
+        });
+        return false;
+    })
+    .parent()
+    .buttonset()
+    .next()
+    .hide()
+    .menu();  
+};
